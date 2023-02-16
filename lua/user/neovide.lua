9a543bc389600a128a2b35f1c8558611edabf301
201dreamers <201dreamers@gmail.com>
2023-02-16 20:28:36 +0200

    remove neovide config file

--- lua/user/neovide.lua
+++
@@ -1,7 +0,0 @@
-vim.g.neovide_transparency = 1.0
-vim.g.neovide_fullscreen = false
-vim.g.neovide_remember_window_size = true
-vim.g.neovide_profiler = false
-vim.g.neovide_cursor_animation_length = 0.05
-vim.g.neovide_cursor_trail_size = 0.5
-vim.g.neovide_scale_factor = 1.0
